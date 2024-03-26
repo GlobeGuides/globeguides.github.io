8ee1849e97b17cc1690483e65b03d925d78d0e1f
Teddy0008 <globeguides.gg@gmail.com>
2024-03-26 17:13:20 +0100

    Update login.js

--- script/login.js
+++ script/login.js
@@ -3,10 +3,10 @@ function login() {
     var password = document.getElementById("password").value;
     
     // Check if username and password are correct (you can replace this with your authentication logic)
-    if (username === "Teddy0008" || "teddy0008" && password === "JuliaIsBest123") {
+    if (username === "Teddy0008" && password === "JuliaIsBest123") {
         // Redirect to another page
         window.location.href = "dashboard/teddy0008"; // Replace "another_page.html" with your desired page
-    } else if (username === "jurkopp4" || "Jurkopp4" && password === "password") {
+    } else if (username === "jurkopp4" && password === "qx5KtvV7") {
         // Redirect to another page
         window.location.href = "dashboard/jurkopp4"; // Replace "another_page.html" with your desired page
     } else {
